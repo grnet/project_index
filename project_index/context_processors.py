67cb0f9d5195de264160d0b4776dbabb222bb350
Kroustouris Stauros <staurosk@noc.grnet.gr>
2015-06-29 12:32:06 +0300

    add menu mechanism

--- project_index/context_processors.py
+++ project_index/context_processors.py
@@ -13,9 +13,23 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from django.conf import settings
+from django.core.urlresolvers import reverse
 
 
 def branding(request):
         return {
             'branding': settings.BRANDING,
         }
+
+
+def menu(request):
+    path = request.path
+    menu = [
+        {'url': reverse('notes:list'), 'name': 'Notes'},
+        {'url': reverse('list'), 'name': 'Projects'},
+        {'url': reverse('tags:list'), 'name': 'Tags'},
+    ]
+    for m in menu:
+        if m.get('url') == path:
+            m.update({'active': 'active'})
+    return {'menu': menu}
--- project_index/settings.py
+++ project_index/settings.py
@@ -111,7 +111,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django.contrib.messages.context_processors.messages',
-    'project_index.context_processors.branding'
+    'project_index.context_processors.branding',
+    'project_index.context_processors.menu',
 )
 
 AUTH_PROFILE_MODULE = 'accounts.UserProfile'
