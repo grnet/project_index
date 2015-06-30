a41926c820c9249fef95e3126466666775b0612c
Kroustouris Stauros <staurosk@noc.grnet.gr>
2015-06-30 16:10:55 +0300

    dependency version

--- index/models.py
+++ index/models.py
@@ -92,6 +92,7 @@ class Dependency(models.Model):
     name = models.CharField(max_length=255)
     package_name = models.SlugField(max_length=255, null=True)
     pip_package_name = models.SlugField(max_length=255)
+    version = models.CharField(max_length=255)
 
     def __unicode__(self):
         return self.name
