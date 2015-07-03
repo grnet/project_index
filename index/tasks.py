b8995143ca6711fb8379793d9347ff12d628ea4a
Kroustouris Stauros <staurosk@noc.grnet.gr>
2015-07-03 10:54:36 +0300

    debug tasks

--- index/tasks.py
+++ index/tasks.py
@@ -48,5 +48,4 @@ def get_requirements(project):
         for l in project.dependency_file.readlines():
             parse_dependencies(l, project)
         return True
-    else:
-        return False
+    return False
