b6916d1fa29de3d4e625df76ba085872f2184014
Kroustouris Stauros <staurosk@noc.grnet.gr>
2015-07-09 12:43:06 +0300

    add admin classes for all models with search

--- index/admin.py
+++ index/admin.py
@@ -39,6 +39,7 @@ class CronjobInline(admin.TabularInline):
 
 
 class ProjectAdmin(admin.ModelAdmin):
+    search_fields = ['name']
     prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'hosts')
     list_filter = ('tag',)
@@ -51,12 +52,32 @@ class ProjectAdmin(admin.ModelAdmin):
     ]
 
 
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+class HostAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+class CronjobAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_filter = ('project', 'hosts')
+    list_display = ('name', 'period', 'user', 'command', 'host_list')
+
+
+class VirtualenvAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
 class DependencyAdmin(admin.ModelAdmin):
+    search_fields = ['name']
     list_display = ('name', 'version', 'dependent_project_count')
 
+
 admin.site.register(Project, ProjectAdmin)
-admin.site.register(Tag)
-admin.site.register(Host)
-admin.site.register(Cronjob)
-admin.site.register(Virtualenv)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Host, HostAdmin)
+admin.site.register(Cronjob, CronjobAdmin)
+admin.site.register(Virtualenv, VirtualenvAdmin)
 admin.site.register(Dependency, DependencyAdmin)
