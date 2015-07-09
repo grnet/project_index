6896c76f580c1cd8662ae4f5c6842d864d234c5a
Kroustouris Stauros <staurosk@noc.grnet.gr>
2015-07-09 12:47:04 +0300

    project is required in notes

--- notes/models.py
+++ notes/models.py
@@ -8,7 +8,7 @@ class Note(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     tag = models.ManyToManyField(Tag)
-    project = models.ForeignKey(Project, null=True, blank=True)
+    project = models.ForeignKey(Project)
 
     def __unicode__(self):
         return self.title
